2c8c22b9c8fc3df5d00da8b36ddcd2a0caa13c2d
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2022-06-10 17:30:42 -0300

    arquivos atualizados

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # troca-de-tema
- 
+
+Projeto com o intuito de praticar minhas habilidades com HTML, CSS e JS
+
+**O projeto ainda não está pronto, será necessário adicionar animações ao botão**
--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function verificar(){
         fundo.style.background = 'rgb(59, 59, 59)'
         main.style.background = 'grey'
         titulo.style.color = 'white'
-        titulo.style.borderBottomColor = 'blue' //troca cor da borda debaixo do titulo
+        titulo.style.borderBottomColor = 'blue'
         titulo.innerText = 'Tema escuro'
         paragrafo.style.color = 'white'
         ball.classList.remove('esquerda')
@@ -20,7 +20,7 @@ function verificar(){
         fundo.style.background = 'rgb(167, 166, 166)'
         main.style.background = 'white'
         titulo.style.color = 'black'
-        titulo.style.borderBottomColor = 'rgb(43, 167, 250)' //troca cor da borda debaixo do titulo
+        titulo.style.borderBottomColor = 'rgb(43, 167, 250)'
         titulo.innerText = 'Tema claro'
         paragrafo.style.color = 'black'
         ball.classList.remove('direita')
